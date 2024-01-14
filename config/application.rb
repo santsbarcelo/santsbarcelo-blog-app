3e9b8b34cbf22abd0517281cb57d502785e59f95
{santsbarcelo} <uuchan2011@gmail.com>
2024-01-14 15:00:44 +0900

    dotenvに関するバグを修正

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module BlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
